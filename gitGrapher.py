b637000ffb8abe53d258465044c4468cfe953195
Aaron Falloon <aaron.falloon@typecast.com>
2014-07-29 17:41:07 +0100

    Get all the branches

--- gitGrapher.py
+++ gitGrapher.py
@@ -1,9 +1,14 @@
 import sys
 import argparse
+import git
+import networkx
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path', metavar='P', help='A path to a directory which contains a Git repo')
 
 args = parser.parse_args()
 
+repo = git.Repo(args.path)
 
+# Get all the branches
+print repo.heads
